36e5f487f54fc822965f90818bf9c692fb158fc6
ginomassei <ginomassei@icloud.com>
2023-07-10 14:38:43 -0300

    tests changed

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,8 +6,8 @@ describe('Login validations', () => {
     cy.visit('/login')
   })
 
-  const usr = 'juan@prueba.com'
-  const psw = 'Prueba123'
+  const usr = 'something@something.com'
+  const psw = 'valid-password'
 
   it('Invalid email', () => {
 
@@ -16,14 +16,14 @@ describe('Login validations', () => {
     cy.get('[data-cy=email]').type('fsakgfask@fajskhsa.com')
     cy.get('[data-cy=password]').type(psw)
     cy.get('[data-cy=submit]').click()
-    
+
     cy.wait('@authResponse').then((interception) => {
 
       expect(interception.response.statusCode).to.eq(401)
       expect(interception.response.body.message).to.contain('User not found')
 
     })
-    
+
   })
 
   it('Invalid password', () => {
@@ -33,54 +33,54 @@ describe('Login validations', () => {
     cy.get('[data-cy=email]').type(usr)
     cy.get('[data-cy=password]').type('falds')
     cy.get('[data-cy=submit]').click()
-    
+
     cy.wait('@authResponse').then((interception) => {
 
       expect(interception.response.statusCode).to.eq(401)
       expect(interception.response.body.message).to.contain('Invalid password')
 
     })
-    
+
   })
 
   it('Email not entered', () => {
 
     cy.get('[data-cy=password]').type(psw)
     cy.get('[data-cy=submit]').click()
-    
+
     cy.get('[data-cy=missing-email]').should('be.visible')
-    
+
   })
 
   it('Password not entered', () => {
-    
+
     cy.get('[data-cy=email]').type(usr)
     cy.get('[data-cy=submit]').click()
-    
+
     cy.get('[data-cy=missing-password]').should('be.visible')
-    
+
   })
 
   it('Email and password not entered', () => {
-    
+
     cy.get('[data-cy=submit]').click()
-    
+
     cy.get('[data-cy=missing-email]').should('be.visible')
     cy.get('[data-cy=missing-password]').should('be.visible')
-    
+
   })
 
   it('Show and hide password', () => {
 
     cy.get('[data-cy=password]').type(psw)
     cy.get('[data-cy=show-password]').click()
-    
+
     cy.get('[data-cy=password]').should('have.attr', 'type', 'text')
 
     cy.get('[data-cy=show-password]').click()
 
     cy.get('[data-cy=password]').should('have.attr', 'type', 'password')
-    
-  })
 
   })
+
+})
